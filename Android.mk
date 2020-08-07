834026dce9e77d605583200086669b5dc7765f8c
Luke Williams <expressluke@turbox.uk>
2020-08-07 21:58:27 +0100

    AOSP-ify Hardware Samsung

--- Android.mk
+++ Android.mk
@@ -41,9 +41,9 @@ include $(SAM_ROOT)/loki_tool/Android.mk
 endif
 
 ifeq ($(BOARD_VENDOR),samsung)
-include $(SAM_ROOT)/AdvancedDisplay/Android.mk
+#include $(SAM_ROOT)/AdvancedDisplay/Android.mk
 include $(SAM_ROOT)/audio/Android.mk
-include $(SAM_ROOT)/doze/Android.mk
+#include $(SAM_ROOT)/doze/Android.mk
 include $(SAM_ROOT)/dtbhtool/Android.mk
 include $(SAM_ROOT)/hidl/Android.mk
 include $(SAM_ROOT)/modemloader/Android.mk
--- hidl/power/Power.cpp
+++ hidl/power/Power.cpp
@@ -105,8 +105,7 @@ Return<void> Power::powerHint(PowerHint hint, int32_t data) {
     }
 
     /* Bail out if low-power mode is active */
-    if (current_profile == PowerProfile::POWER_SAVE && hint != PowerHint::LOW_POWER &&
-        hint != static_cast<PowerHint>(LineagePowerHint::SET_PROFILE)) {
+    if (current_profile == PowerProfile::POWER_SAVE && hint != PowerHint::LOW_POWER) {
         LOG(VERBOSE) << "PROFILE_POWER_SAVE active, ignoring hint " << static_cast<int32_t>(hint);
         return Void();
     }
@@ -120,13 +119,6 @@ Return<void> Power::powerHint(PowerHint hint, int32_t data) {
             setProfile(data ? PowerProfile::POWER_SAVE : PowerProfile::BALANCED);
             break;
         default:
-            if (hint == static_cast<PowerHint>(LineagePowerHint::SET_PROFILE)) {
-                setProfile(static_cast<PowerProfile>(data));
-            } else if (hint == static_cast<PowerHint>(LineagePowerHint::CPU_BOOST)) {
-                sendBoost(data);
-            } else {
-                LOG(INFO) << "Unknown power hint: " << static_cast<int32_t>(hint);
-            }
             break;
     }
     return Void();
@@ -149,15 +141,6 @@ Return<void> Power::setFeature(Feature feature __unused, bool activate __unused)
 Return<void> Power::getPlatformLowPowerStats(getPlatformLowPowerStats_cb _hidl_cb) {
     _hidl_cb({}, Status::SUCCESS);
     return Void();
-}
-
-Return<int32_t> Power::getFeature(LineageFeature feature) {
-    switch (feature) {
-        case LineageFeature::SUPPORTED_PROFILES:
-            return static_cast<int32_t>(PowerProfile::MAX);
-        default:
-            return -1;
-    }
 }
 
 void Power::initialize() {
